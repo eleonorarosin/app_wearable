16c54d080a32f870822ec46d1bd56eb030cde3f0
mariachiara3 <mariachiara.vardeu@studenti.unipd.it>
2023-05-08 10:06:49 +0200

    aggiunta icona

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'home.dart';
+//import 'home.dart';
 
 class Splash extends StatelessWidget {
   static const route = '/splash/';
@@ -8,25 +8,25 @@ class Splash extends StatelessWidget {
 
   const Splash({Key? key}) : super(key: key);
 
-  // Method for navigation SplashPage -> HomePage
-  void _toHomePage(BuildContext context) {
-    Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
-  } //_toHomePage
+// Method for navigation SplashPage -> HomePage
+// void _toHomePage(BuildContext context) {
+//   Navigator.of(context)
+//      .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
+//} //_toHomePage
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () => _toHomePage(context));
+    //Future.delayed(const Duration(seconds: 3), () => _toHomePage(context));
     return Material(
       child: Container(
-        color: const Color(0xFF83AA99),
+        color: Color.fromARGB(255, 213, 243, 230),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: const <Widget>[
             Text(
-              'PolluTrack',
+              'GreenSteps',
               style: TextStyle(
-                  color: Color(0xFFE4DFD4),
+                  color: Color.fromARGB(255, 20, 134, 37),
                   fontSize: 40,
                   fontWeight: FontWeight.bold),
             ),
