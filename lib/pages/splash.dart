ca8d8af2467f25aabdfecc3d3896e276c050d620
mariachiara3 <mariachiara.vardeu@studenti.unipd.it>
2023-05-08 10:08:40 +0200

    ui

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
 
-//import 'home.dart';
-
 class Splash extends StatelessWidget {
   static const route = '/splash/';
   static const routeDisplayName = 'SplashPage';
 
   const Splash({Key? key}) : super(key: key);
 
-// Method for navigation SplashPage -> HomePage
-// void _toHomePage(BuildContext context) {
-//   Navigator.of(context)
-//      .pushReplacement(MaterialPageRoute(builder: (context) => Home()));
-//} //_toHomePage
-
   @override
   Widget build(BuildContext context) {
-    //Future.delayed(const Duration(seconds: 3), () => _toHomePage(context));
     return Material(
       child: Container(
         color: Color.fromARGB(255, 213, 243, 230),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const <Widget>[
+          children: <Widget>[
             Text(
               'GreenSteps',
               style: TextStyle(
@@ -30,11 +21,15 @@ class Splash extends StatelessWidget {
                   fontSize: 40,
                   fontWeight: FontWeight.bold),
             ),
-            Center(
-              child: CircularProgressIndicator(
-                strokeWidth: 4,
-                valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF89453C)),
-              ),
+            Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Icon(Icons.energy_savings_leaf),
+                CircularProgressIndicator(
+                  strokeWidth: 8,
+                  valueColor: AlwaysStoppedAnimation<Color>(Color(0xFF89453C)),
+                ),
+              ],
             ),
           ],
         ),
