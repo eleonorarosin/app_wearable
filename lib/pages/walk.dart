5d8f391c13abbb4d48bd74eb25fad4b7619da256
mariachiara3 <mariachiara.vardeu@studenti.unipd.it>
2023-05-23 11:51:51 +0200

    grafico2

--- lib/pages/walk.dart
+++ lib/pages/walk.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 List<Map<String, dynamic>> data = [
-  {'date': '2021-10-01', 'points': 1468},
-  {'date': '2021-10-01', 'points': 1487},
-  {'date': '2021-10-01', 'points': 1494},
-  {'date': '2021-10-02', 'points': 1526},
-  {'date': '2021-10-02', 'points': 1492},
-  {'date': '2021-10-02', 'points': 1470},
-  {'date': '2021-10-02', 'points': 1477},
-  {'date': '2021-10-03', 'points': 1466},
-  {'date': '2021-10-03', 'points': 1465},
-  {'date': '2021-10-03', 'points': 1524},
-  {'date': '2021-10-03', 'points': 1534},
-  {'date': '2021-10-04', 'points': 1504},
-  {'date': '2021-10-04', 'points': 1524},
-  {'date': '2021-10-05', 'points': 1534},
-  {'date': '2021-10-06', 'points': 1463},
+  {'date': '2023-05-23', 'points': 1468},
+  {'date': '2023-05-23', 'points': 1487},
+  {'date': '2023-05-23', 'points': 1494},
+  {'date': '2023-05-22', 'points': 1526},
+  {'date': '2023-05-22', 'points': 1492},
+  {'date': '2023-05-22', 'points': 1470},
+  {'date': '2023-05-22', 'points': 1477},
+  {'date': '2023-05-21', 'points': 1466},
+  {'date': '2023-05-21', 'points': 1465},
+  {'date': '2023-05-21', 'points': 1524},
+  {'date': '2023-05-21', 'points': 1534},
+  {'date': '2023-05-20', 'points': 1504},
+  {'date': '2023-05-20', 'points': 1524},
+  {'date': '2023-05-19', 'points': 1534},
+  {'date': '2023-05-19', 'points': 1463},
   {'date': '2021-10-07', 'points': 1502},
   {'date': '2021-10-07', 'points': 1539},
   {'date': '2021-10-08', 'points': 1476},
@@ -60,6 +59,7 @@ class Walk extends StatefulWidget {
 class _WalkState extends State<Walk> {
   double exposure = 2.5;
   DateTime day = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -71,9 +71,10 @@ class _WalkState extends State<Walk> {
           const Text(
             'Daily Activity',
             style: TextStyle(
-                color: Color.fromARGB(255, 8, 112, 24),
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
+              color: Color.fromARGB(255, 8, 112, 24),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           const SizedBox(
             height: 20,
@@ -81,47 +82,14 @@ class _WalkState extends State<Walk> {
           const Text(
             'Exposure Trend',
             style: TextStyle(
-                color: Color.fromARGB(255, 8, 112, 24),
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+              color: Color.fromARGB(255, 8, 112, 24),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           const SizedBox(
             height: 20,
           ),
-          /*Align(
-            alignment: Alignment.center,
-            child: SizedBox(
-              width: 150,
-              height: 150,
-              child: CustomPaint(
-                painter: SfCircularChart(
-                  legend: Legend(isVisible: true)
-                  
-                ),
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Center(
-                        child: Padding(
-                      padding: const EdgeInsets.only(top: 40.0),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text(
-                              '$exposure',
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Color.fromARGB(255, 8, 112, 24)),
-                            ),
-                            const Text(
-                              'Low',
-                              style: TextStyle(fontSize: 16),
-                            )
-                          ]),
-                    ))),
-              ),
-            ),
-          ),*/
           Align(
             alignment: Alignment.center,
             child: SizedBox(
@@ -170,9 +138,10 @@ class _WalkState extends State<Walk> {
           const Text(
             'Weekly Trend',
             style: TextStyle(
-                color: Color.fromARGB(255, 8, 112, 24),
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
+              color: Color.fromARGB(255, 8, 112, 24),
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           const SizedBox(
             height: 15,
@@ -181,23 +150,47 @@ class _WalkState extends State<Walk> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                  icon: const Icon(Icons.navigate_before),
-                  onPressed: () {
-                    setState(() {
-                      day = day.subtract(const Duration(days: 1));
-                    });
-                  }),
+                icon: const Icon(Icons.navigate_before),
+                onPressed: () {
+                  setState(() {
+                    day = day.subtract(const Duration(days: 1));
+                  });
+                },
+              ),
               Text(DateFormat('dd MMMM yyyy').format(day)),
               IconButton(
-                  icon: const Icon(Icons.navigate_next),
-                  onPressed: () {
-                    setState(() {
-                      day = day.add(const Duration(days: 1));
-                    });
-                  })
+                icon: const Icon(Icons.navigate_next),
+                onPressed: () {
+                  setState(() {
+                    day = day.add(const Duration(days: 1));
+                  });
+                },
+              ),
             ],
           ),
-          //CustomPlot(data: data)
+          SizedBox(
+            height: 200,
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              primaryYAxis: NumericAxis(),
+              series: <ChartSeries>[
+                ColumnSeries<Map<String, dynamic>, String>(
+                  dataSource: data
+                      .where(
+                        (element) =>
+                            DateTime.parse(element['date']).isAfter(
+                                day.subtract(const Duration(days: 7))) &&
+                            DateTime.parse(element['date']).isBefore(day),
+                      )
+                      .toList(),
+                  xValueMapper: (Map<String, dynamic> data, _) =>
+                      data['date'].toString(),
+                  yValueMapper: (Map<String, dynamic> data, _) =>
+                      data['points'],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
