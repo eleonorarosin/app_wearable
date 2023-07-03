62eb87d92bdfb5fda3b9be008ef8cdf4d919a067
eleonorarosin <eleonora.rosin@studenti.unipd.it>
2023-07-03 19:06:02 +0200

    commenti

--- lib/models/daos/distances_dao.dart
+++ lib/models/daos/distances_dao.dart
@@ -9,24 +9,6 @@ abstract class FootDistancesDao {
   @Query('SELECT * FROM FootDistances WHERE dateTime between :startTime and :endTime ORDER BY dateTime ASC')
   Future<List<FootDistances>> findDistancesbyDate(DateTime startTime, DateTime endTime);
   
-  /*Future<int> findDistancesbyDate(DateTime startTime, DateTime endTime) async {
-  final List<FootDistances> distList = await _findDistancesbyDate(startTime, endTime);
-  int sum = 0;
-  for (final dist in distList) {
-    sum += dist.value;
-  }
-  return sum;
-  }*/
-  /*Future<List<FootDistances>> findDistancesbyDate(DateTime startTime, DateTime endTime) async {
-  final List<FootDistances> distList = await _findDistancesbyDate(startTime, endTime);
-  int sum = 0;
-  for (final dist in distList) {
-    sum += dist.value;
-  }
-  return [distList, sum];
-}*/
-
-
   //Query #1: SELECT -> this allows to obtain all the entries of the HR table
   @Query('SELECT * FROM FootDistances')
   Future<List<FootDistances>> findAllDistances();
